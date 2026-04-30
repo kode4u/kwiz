381e054907887c987853875badf795923e865465
kode4u <info.kode4u@gmail.com>
2026-04-30 18:05:53 +0700

    OK

--- docker/moodle/docker-entrypoint.sh
+++ docker/moodle/docker-entrypoint.sh
@@ -9,5 +9,11 @@ fi
 chown -R www-data:www-data /var/www/moodledata
 chmod -R 0777 /var/www/moodledata
 
+# If config.php exists (e.g. created by CLI installer), make it readable by Apache.
+if [ -f /var/www/html/config.php ]; then
+    chown www-data:www-data /var/www/html/config.php
+    chmod 0640 /var/www/html/config.php
+fi
+
 # Execute the original entrypoint with the command
 exec docker-php-entrypoint "$@"
