61832ba53019ae5cbfb75add287cbfffd6f6aea2
kode4u <info.kode4u@gmail.com>
2026-09-18 17:07:02 +0900

    Smooth unhurried 160ms cascade with 0.8s quintic ease (v1.0.62)

--- h5p_smooth_build/H5P.RiseCourse-1.0/scripts/rise-course.js
+++ h5p_smooth_build/H5P.RiseCourse-1.0/scripts/rise-course.js
@@ -1426,8 +1426,8 @@ H5P.RiseCourse = (function ($, EventDispatcher) {
             if (el && !el.classList.contains("is-visible")) {
               el.classList.add("is-visible");
             }
-          }, idx * 90);
-          delay = idx * 90;
+          }, idx * 160);
+          delay = idx * 160;
         });
 
         setTimeout(function () {
@@ -1435,7 +1435,7 @@ H5P.RiseCourse = (function ($, EventDispatcher) {
           if (revealQueue.length > 0) {
             drainRevealQueue();
           }
-        }, delay + 60);
+        }, delay + 80);
       };
 
       self.enqueueRevealItem = function (domEl) {
@@ -1445,7 +1445,7 @@ H5P.RiseCourse = (function ($, EventDispatcher) {
         }
         if (!isProcessingQueue) {
           // Micro-tick to batch elements entering at the same moment
-          setTimeout(drainRevealQueue, 25);
+          setTimeout(drainRevealQueue, 35);
         }
       };
 
